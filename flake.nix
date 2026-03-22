09d4a2bd2fc32ee0358040b01dbfb90e30a876a2
James Laird-Smith <jameslairdsmith@gmail.com>
2026-03-22 15:12:39 +0000

    Nix LSP

--- flake.nix
+++ flake.nix
@@ -5,34 +5,34 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      system = "aarch64-darwin"; # Use "x86_64-darwin" for Intel Macs
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true; # This is the magic line
-      };
-
-      myVscode = pkgs.vscode-with-extensions.override {
-        vscodeExtensions = with pkgs.vscode-extensions; [
-          jnoortheen.nix-ide # Nix language support + formatting
-          sourcegraph.amp
-        ];
-      };
-    in
-    {
-
-      devShells."aarch64-darwin".default = pkgs.mkShell {
-        packages = [
-          pkgs.nixfmt
-          pkgs.alejandra
-          myVscode
-        ];
-        shellHook = ''
-          echo "welcome to the shell!"
-        '';
-        NIXPKGS_ALLOW_UNFREE = 1;
-      };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "aarch64-darwin"; # Use "x86_64-darwin" for Intel Macs
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true; # This is the magic line
     };
+
+    myVscode = pkgs.vscode-with-extensions.override {
+      vscodeExtensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide # Nix language support + formatting
+        sourcegraph.amp
+      ];
+    };
+  in {
+    devShells."aarch64-darwin".default = pkgs.mkShell {
+      packages = [
+        pkgs.nixfmt
+        pkgs.alejandra
+        pkgs.nixd
+        myVscode
+      ];
+      shellHook = ''
+        echo "welcome to the shell!"
+      '';
+      NIXPKGS_ALLOW_UNFREE = 1;
+    };
+  };
 }
