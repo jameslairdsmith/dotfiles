ec5dd74671968f263757ecc9e9d532fd1c11d910
James Laird-Smith <jameslairdsmith@gmail.com>
2026-08-11 17:02:45 +0100

    chore(home-manager): Remove commented package examples
    Clean up `home.nix` by removing unused commented-out examples for
    package overrides and custom shell scripts.

--- nix/hosts/neo/home.nix
+++ nix/hosts/neo/home.nix
@@ -35,19 +35,6 @@ in
     amp-cli
     onefetch
     zoom-us
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   home.file = {
